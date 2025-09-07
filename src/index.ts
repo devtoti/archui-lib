c7846ae73dd7f4c56a6f2876bfdd94cb749b09e2
Antonio <toti.webdev@gmail.com>
2025-09-07 14:45:29 -0600

    1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 // Export all components
-export { Button } from './components/Button/Button';
-export type { ButtonProps } from './components/Button/Button';
+export { Button, type ButtonProps } from './components/Button/Button';
+export { ThemeProvider } from './components/ThemeProvider';
+
+// Export hooks
+export { useTheme } from './hooks/useTheme';
+
+// Export contexts and types
+export { ThemeContext, type Theme, type ThemeContextType } from './contexts/ThemeContext';
 
 // Export styles
 import './index.css';
