bd5d7d32d2a5d63e1a1782f2fff4e916ecb30538
Antonio <toti.webdev@gmail.com>
2025-12-28 16:20:21 -0600

    Add features desktop

--- website/src/react/components/ReactComponentsPreview.tsx
+++ website/src/react/components/ReactComponentsPreview.tsx
@@ -4,7 +4,10 @@ import { ThemeProvider } from '@devtoti/archui-lib';
 
 export default function ReactComponentsPreview() {
   return (
-    <ThemeProvider theme="doric" className="w-full h-full">
+    <ThemeProvider
+      theme="doric"
+      className="w-full h-full flex flex-col items-center justify-center gap-8 md:flex-row"
+    >
       <Button variant="primary" size="md" label="Welcome to ArchUI!" />
       <LinkItem href="https://www.google.com" label="Google" />
       <Switch variant="default" size="md" />
