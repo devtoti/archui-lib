622fad8c88440ee0c0dccf89b525d31786cfbbb3
Antonio <toti.webdev@gmail.com>
2025-12-28 19:00:28 -0600

    Add carrousel

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://archui.devtoti.com/",
   base: "",
   trailingSlash: "always",
-  integrations: [react()],
+  integrations: [react(), svelte()],
   vite: {
     plugins: [tailwindcss()],
   },
---
+++ website/src/components/Footer.svelte
@@ -0,0 +1,472 @@
+<footer
+  class="footer text-sys-secondary border-t-2 border-sys-brd-primary flex flex-col lg:grid lg:grid-cols-12"
+  id="footer"
+>
+  <div
+    class="h-24 row-start-1 row-end-2 flex flex-row justify-center items-center w-full col-span-full mt-6"
+  >
+    <svg
+      width="82"
+      height="81"
+      viewBox="0 0 82 81"
+      role="presentation"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="40.9992"
+        cy="40.6867"
+        r="28.08"
+        stroke="#2855AD"
+        stroke-width="1.44"
+      />
+      <path
+        d="M41.0006 6.34295C36.4904 6.34295 32.0245 7.23129 27.8577 8.95723C23.6909 10.6832 19.9048 13.2129 16.7157 16.4021C13.5265 19.5912 10.9968 23.3773 9.27083 27.5441C7.54489 31.7109 6.65655 36.1768 6.65655 40.687"
+        stroke="#2855AD"
+        stroke-width="0.432"
+      />
+      <path
+        d="M40.9994 75.032C45.5096 75.032 49.9755 74.1437 54.1423 72.4178C58.3091 70.6918 62.0952 68.1621 65.2843 64.9729C68.4735 61.7838 71.0032 57.9977 72.7292 53.8309C74.4551 49.6641 75.3434 45.1982 75.3434 40.688"
+        stroke="#2855AD"
+        stroke-width="0.432"
+      />
+      <circle
+        cx="40.9996"
+        cy="40.6871"
+        r="14.04"
+        stroke="#2855AD"
+        stroke-width="0.72"
+      />
+      <line
+        x1="0.679688"
+        y1="40.3375"
+        x2="35.2397"
+        y2="40.3375"
+        stroke="#4774CC"
+        stroke-width="0.7"
+      />
+      <line
+        x1="46.7598"
+        y1="40.3375"
+        x2="81.3198"
+        y2="40.3375"
+        stroke="#4774CC"
+        stroke-width="0.7"
+      />
+      <line
+        x1="41.349"
+        y1="0.367188"
+        x2="41.349"
+        y2="34.9272"
+        stroke="#4774CC"
+        stroke-width="0.7"
+      />
+      <line
+        x1="41.349"
+        y1="46.4473"
+        x2="41.349"
+        y2="81.0073"
+        stroke="#4774CC"
+        stroke-width="0.7"
+      />
+      <path
+        d="M44.0295 36.6504H45.1527L43.9863 37.8888C44.6055 38.58 44.9367 39.4584 44.9367 40.4376V40.4664C44.9367 42.5832 43.2807 44.3544 41.0343 44.3544C40.1127 44.3544 39.3351 44.0664 38.7015 43.5912L38.0679 44.2536H36.9591L38.0967 43.0296C37.5063 42.3528 37.1751 41.4744 37.1751 40.4952V40.4664C37.1751 38.3496 38.8023 36.5784 41.0631 36.5784C41.9703 36.5784 42.7479 36.8664 43.3815 37.3416L44.0295 36.6504ZM41.0631 43.3896C42.7047 43.3896 43.8135 42.0648 43.8135 40.4952V40.4808C43.8135 39.7896 43.6263 39.1848 43.2519 38.6808L39.3927 42.828C39.8535 43.188 40.4295 43.404 41.0631 43.3896ZM38.2839 40.4376V40.452C38.2839 41.1288 38.4855 41.748 38.8455 42.252L42.7047 38.1192C42.2439 37.7592 41.6679 37.5432 41.0343 37.5432C39.4071 37.5432 38.2839 38.868 38.2839 40.4376Z"
+        fill="#4774CC"
+      />
+    </svg>
+  </div>
+
+  <article class="nav-links row-start-2 row-end-3">
+    <article
+      class="navigation-left"
+      role="navigation"
+      aria-label="Primary navigation links"
+    >
+      <ul>
+        <h6 class="arc-h4">Contact</h6>
+        <li>
+          <a href="/" class="arc-body-1">Storybook</a>
+        </li>
+        <li>
+          <a href="/#work-experience" class="arc-body-1">GitHub</a>
+        </li>
+        <li>
+          <a href="/#projects" class="arc-body-1">Dribbble</a>
+        </li>
+        <li>
+          <a href="/#skills" class="arc-body-1">LinkedIn</a>
+        </li>
+        <li>
+          <a href="/#services" class="arc-body-1">Portfolio</a>
+        </li>
+      </ul>
+    </article>
+    <article
+      class="navigation-right"
+      role="navigation"
+      aria-label="Secondary navigation links"
+    >
+      <ul>
+        <h6 class="arc-h4">Documentation</h6>
+        <li>
+          <a href="/about" class="arc-body-1">Get Started</a>
+        </li>
+        <li>
+          <a href="/dev" class="arc-body-1">Docs</a>
+        </li>
+        <li>
+          <a href="/design" class="arc-body-1">Setup</a>
+        </li>
+        <li>
+          <a href="/concept" class="arc-body-1">Themes</a>
+        </li>
+        <li>
+          <a href="/concept" class="arc-body-1">Contact</a>
+        </li>
+      </ul>
+    </article>
+  </article>
+  <div class="footer-container lg:w-3/5 mx-auto row-start-2 row-end-3">
+    <article class="plan-info techs">
+      <span class="arc-h5">Tech Stack</span>
+      <p class="arc-body-2">React, Tailwind, TypeScript</p>
+    </article>
+    <article class="plan-info proj">
+      <h6 class="arc-h5">Project</h6>
+      <p class="arc-body-2">ArchUI Component Library</p>
+    </article>
+    <article class="plan-info loc">
+      <h6 class="arc-h5">Location</h6>
+      <p class="arc-body-2">Mexico City</p>
+    </article>
+    <article class="plan-info scale">
+      <h6 class="arc-h5">Scale</h6>
+      <div id="scale">
+        <div class="scale-top">
+          <span class="arc-h5"></span>
+          <span class="arc-h5"></span>
+          <span class="arc-h5"></span>
+          <span class="arc-h5"></span>
+          <span class="arc-h5"></span>
+        </div>
+        <div class="scale-bottom">
+          <span class="arc-h5"></span>
+          <span class="arc-h5"></span>
+          <span class="arc-h5"></span>
+          <span class="arc-h5"></span>
+          <span class="arc-h5"></span>
+        </div>
+        <!-- <ul>
+          <li class="arc-h5">1</li>
+          <li class="arc-h5">2.5</li>
+          <li class="arc-h5">5</li>
+          <li class="arc-h5">10</li>
+          <li class="arc-h5">20</li>
+        </ul> -->
+      </div>
+    </article>
+    <article class="plan-info rev">
+      <h6 class="arc-h5">Last Update</h6>
+      <p class="arc-body-2">December 28, 2025</p>
+    </article>
+    <article class="plan-info collab">
+      <h6 class="arc-h5">Collaborators</h6>
+      <p class="arc-body-2">@devtoti</p>
+    </article>
+    <article class="plan-info author">
+      <h6 class="arc-h5">Architect</h6>
+      <p class="arc-body-2">@devtoti</p>
+    </article>
+    <article class="plan-info v">
+      <h4 class="arc-h3">V1.1.0</h4>
+    </article>
+    <article class="plan-info id" id="clave">
+      <h5 class="arc-h0">V1</h5>
+    </article>
+  </div>
+</footer>
+
+<style lang="scss">
+  /* footer {
+    background-image:
+      linear-gradient(to right, var(--gray-alpha-05) 1px, transparent 1px),
+      linear-gradient(to bottom, var(--gray-alpha-05) 1px, transparent 1px);
+    background-size: 16px 16px;
+    background-position: -5px -5px;
+  } */
+  h6 {
+    color: var(--sys-txt-primary) !important;
+  }
+  a {
+    padding: var(--sys-pd-sm) var(--sys-pd-sm);
+  }
+  .footer-title {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    grid-column: 2 / -2;
+    grid-row: 2 / 3;
+    padding-top: var(--sys-pd-lg);
+    .icons {
+      padding-top: var(--sys-pd-sm);
+      display: flex;
+      opacity: 0.75;
+    }
+  }
+  .footer-padding {
+    grid-column: 1 / -1;
+    grid-row: 3 / 4;
+    height: var(--sys-pd-sm);
+    width: 100%;
+    background-color: var(--sys-txt-light);
+  }
+
+  .radial-deco-wrapper {
+    width: 92px;
+    height: 92px;
+    grid-column: 1 / -1;
+    grid-row: 1 / 2;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .plan-info {
+    outline: var(--sys-stroke-light) solid var(--sys-brd-primary);
+    padding-left: var(--sys-pd-xs);
+    width: 100%;
+    h4 {
+      font-weight: 700;
+      padding-top: var(--sys-pd-xs);
+    }
+    p {
+      padding-bottom: var(--sys-pd-xs);
+    }
+  }
+  .footer {
+    position: relative;
+    padding-bottom: var(--sys-pd-lg);
+    padding: 0;
+    height: fit-content;
+    max-width: 100svw;
+    grid-column: 1 / -1;
+    background-color: var(--sys-bg-primary);
+    padding-bottom: var(--sys-pd-lg);
+  }
+
+  .footer-container {
+    outline: var(--sys-stroke-bold) solid var(--sys-brd-primary);
+    height: fit-content;
+    grid-column: 2 / -2;
+    width: 100%;
+    grid-row: 3 / 4;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: repeat(7, auto);
+    grid-template-areas:
+      'nav nav nav navR navR navR'
+      'nav nav nav navR navR navR'
+      'proj proj proj proj proj proj'
+      'author author author author author author'
+      'techs techs techs techs techs techs'
+      'id id id scale scale scale'
+      'id id id loc loc loc'
+      'id id id v v v'
+      'rev rev rev collab collab collab';
+    gap: 0;
+  }
+  .proj {
+    grid-area: proj;
+  }
+  .author {
+    grid-area: author;
+  }
+  #clave {
+    h4 {
+      padding: 0 !important;
+    }
+  }
+  .id {
+    grid-area: id;
+    display: grid;
+    place-items: center;
+  }
+  .techs {
+    grid-area: techs;
+  }
+  .loc {
+    grid-area: loc;
+  }
+  .collab {
+    grid-area: collab;
+  }
+  .scale {
+    grid-area: scale;
+    min-height: 2rem;
+  }
+  #scale {
+    display: grid;
+    grid-template-columns: 1fr 2fr 2.5fr 5fr 10fr;
+    grid-template-rows: repeat(2, 1fr) 12px;
+    gap: 0;
+    margin: 0 0.5rem;
+    height: 8px;
+
+    li {
+      display: none;
+      height: 4px;
+      width: 100%;
+    }
+    .scale-top {
+      display: grid;
+      min-height: 4px;
+      grid-template-columns: 1fr 2fr 2.5fr 5fr 10fr;
+      grid-column: 1 / -1;
+      grid-row: 1 / 2;
+      span:nth-child(odd) {
+        background-color: var(--sys-brd-primary);
+      }
+    }
+    .scale-bottom {
+      display: grid;
+      min-height: 4px;
+      grid-template-columns: 1fr 2fr 2.5fr 5fr 10fr;
+      grid-column: 1 / -1;
+      grid-row: 2 / 3;
+      span:nth-child(even) {
+        background-color: var(--sys-brd-primary);
+      }
+    }
+    ul {
+      display: contents;
+      li {
+        color: var(--sys-txt-tertiary);
+        font-weight: 400;
+        grid-row: 3 / 4;
+        /* display: inline-block; */
+        width: 100%;
+        text-align: center;
+        &:nth-child(1) {
+          grid-column: 1 / 2;
+        }
+        &:nth-child(2) {
+          grid-column: 2 / 3;
+        }
+        &:nth-child(3) {
+          grid-column: 3 / 4;
+        }
+        &:nth-child(4) {
+          grid-column: 4 / 5;
+        }
+      }
+    }
+  }
+  .rev {
+    grid-area: rev;
+  }
+  .v {
+    grid-area: v;
+  }
+  .navigation-left {
+    grid-area: nav;
+  }
+  .navigation-right {
+    grid-area: navR;
+  }
+  .navigation-left,
+  .navigation-right {
+    width: 140px;
+  }
+  ul {
+    list-style: none;
+    padding-bottom: var(--sys-pd-lg);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+    a {
+      text-decoration: none;
+      color: var(--sys-txt-tertiary);
+      padding: 0;
+      justify-content: flex-end;
+      &:hover {
+        color: var(--sys-brd-primary) !important;
+        font-weight: 500;
+      }
+    }
+  }
+  .navigation-left ul {
+    a {
+      justify-content: flex-end;
+    }
+  }
+  .navigation-right ul {
+    a {
+      justify-content: flex-start;
+    }
+  }
+  .nav-links {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 5rem;
+    margin: var(--sys-pd-lg) 0;
+    width: 100%;
+    grid-column: 1 / -1;
+  }
+  @media (min-width: 480px) {
+    .footer-container {
+      grid-column: 2 / -2;
+      grid-template-areas:
+        'nav nav nav navR navR navR'
+        'nav nav nav navR navR navR'
+        'proj proj proj author author author'
+        'techs techs techs collab collab collab'
+        'id id id loc loc rev'
+        'id id id scale scale v';
+    }
+    #scale {
+      margin: 0 var(--sys-pd-xs);
+      li {
+        display: block !important;
+      }
+    }
+  }
+  @media (min-width: 1029px) {
+    .footer-container {
+      grid-column: 4 / span 6;
+      grid-template-areas:
+        'nav nav nav navR navR navR'
+        'nav nav nav navR navR navR'
+        'id proj proj author author techs'
+        'id loc loc scale scale scale'
+        'id collab collab collab rev v';
+    }
+    .navigation-left {
+      text-align: right;
+    }
+  }
+  :global([data-theme='dark']) {
+    .footer {
+      background-color: var(--sys-bg-secondary);
+    }
+    .plan-info {
+      outline: var(--sys-stroke-light) solid var(--sys-brd-secondary);
+      h4 {
+        color: var(--sys-txt-primary) !important;
+      }
+      p {
+        color: var(--sys-txt-primary) !important;
+      }
+    }
+    .nav-links a {
+      color: var(--sys-txt-primary) !important;
+    }
+    .plan-info.id {
+      h4 {
+        color: var(--sys-txt-light) !important;
+      }
+    }
+  }
+</style>
