bb7c3632b3dc769d061933e0784ffcaa3170a5f9
Antonio <toti.webdev@gmail.com>
2025-12-30 01:06:01 -0600

    Delete astro adapter since it's not needed

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
-import netlify from "@astrojs/netlify";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://archui.devtoti.com/",
   base: "",
   trailingSlash: "always",
-  adapter: netlify(),
+  output: "static",
   integrations: [react(), svelte()],
   vite: {
     plugins: [tailwindcss()],
